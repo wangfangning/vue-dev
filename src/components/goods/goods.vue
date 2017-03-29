e5c0074df2a6dea412399f44be1845d8ab25d175
wangfangning <285132709@qq.com>
2017-03-29 18:21:07 +0800

    good

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -3,12 +3,33 @@
   <div class="menu-wrapper">
     <ul>
       <li v-for="item in goods" class="menu_item">
-        <span class="text"><icon v-show="item.type" :class="classMap[item.type]"></icon>{{item.name}}</span>
+        <span class="text border-1px"><icon v-show="item.type>0" :class="classMap[item.type]"></icon>{{item.name}}</span>
       </li>
     </ul>
   </div>
   <div class="food-wrapper">
+   <ul>
+     <li v-for="item in goods" class="food_list">
+       <h1 class="title">{{item.name}}</h1>
+     <ul>
+       <li v-for="food in item.foods" class="food_item">
+<div class="icon">
+<img :src="food.icon" alt="">
+</div>
+<div class="content">
+  <h2 class="name">{{food.name}}</h2>
+  <p class="desc" v-show="food.description">{{food.description}}</p>
+  <div class="extra">
+<span>月售{{food.sellCount}}</span>
+<span>好评率{{food.rating}}%</span>
+  </div>
 
+
+</div>
+         </li>
+     </ul>
+   </li>
+   </ul>
   </div>
 </div>
 </template>
@@ -42,8 +63,7 @@ export default {
 };
 </script>
 
-<style lang="scss">
-@import "../../common/sass/mixin.scss";
+<style lang="scss">@import "../../common/sass/mixin.scss";
 .goods {
     display: flex;
     position: absolute;
@@ -55,40 +75,42 @@ export default {
         flex: 0 0 80px;
         width: 80px;
         background-color: #f3f5f7;
-        .menu_item{
-          display: table;
-          height: 54px;
-          width: 56px;
-          line-height: 14px;
-          .icon{
-            display: inline-block;
-            width: 12px;
-            height: 12px;
-            margin-right: 2px;
-            background-size: 12px;
-            background-repeat: no-repeat;
-            &.decrease {
-                @include bg-image( 'decrease_3');
+        .menu_item {
+            display: table;
+            height: 54px;
+            width: 56px;
+            line-height: 14px;
+            margin: auto;
+            .icon {
+                display: inline-block;
+                vertical-align: top;
+                width: 12px;
+                height: 12px;
+                margin-right: 2px;
+                background-size: 12px;
+                background-repeat: no-repeat;
+                &.decrease {
+                    @include bg-image( 'decrease_3');
+                }
+                &.discount {
+                    @include bg-image( 'discount_3');
+                }
+                &.guarantee {
+                    @include bg-image( 'guarantee_3');
+                }
+                &.invoice {
+                    @include bg-image( 'invoice_3');
+                }
+                &.special {
+                    @include bg-image( 'special_3');
+                }
             }
-            &.discount {
-                @include bg-image( 'discount_3');
+            .text {
+                display: table-cell;
+                vertical-align: middle;
+                font-size: 12px;
+                @include border-1px(rgba(7,17,27,0.1));
             }
-            &.guarantee {
-                @include bg-image( 'guarantee_3');
-            }
-            &.invoice {
-                @include bg-image( 'invoice_3');
-            }
-            &.special {
-                @include bg-image( 'special_3');
-            }
-          }
-          .text{
-            display: table-cell;
-            vertical-align: middle;
-            font-size: 12px;
-            @include border-1px(rgba(7,17,27,0.1));
-          }
         }
     };
     .food-wrapper {
