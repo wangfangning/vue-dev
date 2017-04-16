ae856db3664cb8a94e7580b459d45bbf2c43cff0
wangfangning <285132709@qq.com>
2017-04-16 22:22:21 +0800

    cart

--- src/components/cartcontrol/cartcontrol.vue
+++ src/components/cartcontrol/cartcontrol.vue
@@ -1,14 +1,11 @@
 <template>
 <div class="cartcontrol">
   <transition name="move">
-  <div class="cart_decrease move-transtion" v-show="food.count>0" @click="decreaseCart">
-<span class="inner icon-remove_circle_outline"></span>
-  </div>
-</transition>
-  <div class="cart_count" v-show="food.count>0">
-  {{food.count}}
-  </div>
-  <div class="cart_add icon-add_circle" @click="addCart">
+    <div class="cart_decrease move-transtion" v-show="food.count>0" @click="decreaseCart">
+      <span class="inner icon-remove_circle_outline"></span></div>
+  </transition>
+  <div class="cart_count" v-show="food.count>0">{{food.count}}</div>
+   <div class="cart_add icon-add_circle" @click="addCart">
   </div>
 </div>
 </template>
@@ -53,25 +50,26 @@ export default {
         display: inline-block;
         padding: 6px;
         transition: all 0.4s linear;
-        &.move-transtion{
-          opacity: 1;
-          transform: translate3d(0,0,0);
-          .inner{
-            display: inline-block;
-            line-height: 24px;
-            font-size: 24px;
-            color: rgb(0,160,220);
-            transition: all 0.4s linear;
-            transform: rotate(0);
-          }
+        &.move-transtion {
+            opacity: 1;
+            transform: translate3d(0,0,0);
+            .inner {
+                display: inline-block;
+                line-height: 24px;
+                font-size: 24px;
+                color: rgb(0,160,220);
+                transition: all 0.4s linear;
+                transform: rotate(0);
+            }
         }
 
-        &.move-enter,&.move-leave-active{
-          opacity: 0;
-          transform: translate3d(24px,0,0);
-          .inner{
-      transform: rotate(180deg);
-          }
+        &.move-enter,
+        &.move-leave-active {
+            opacity: 0;
+            transform: translate3d(24px,0,0);
+            .inner {
+                transform: rotate(180deg);
+            }
         }
     };
     .cart_count {
@@ -82,14 +80,14 @@ export default {
         line-height: 24px;
         text-align: center;
         font-size: 10px;
-        color: rgb(147,153,159)
+        color: rgb(147,153,159);
     };
     .cart_add {
-      display: inline-block;
-      padding: 6px;
-      line-height: 24px;
-      font-size: 24px;
-      color: rgb(0,160,220);
+        display: inline-block;
+        padding: 6px;
+        line-height: 24px;
+        font-size: 24px;
+        color: rgb(0,160,220);
     }
 }
 </style>
