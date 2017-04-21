bba0d476aa024d0bf79fa5ac1c4485a7c3712b08
wangfangning <285132709@qq.com>
2017-04-21 21:20:25 +0800

    food

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -1,24 +1,22 @@
 <template>
 <transition name="move">
-  <div class="food" v-show="showFlag">
+  <div class="food" v-show="showFlag" ref="food">
     <div class="food_content">
       <div class="image_header">
         <img :src="food.image" alt="">
-        <div class="back" @click="show">
+        <div class="back" @click="hide">
           <i class="icon-arrow_lift"></i>
         </div>
 
       </div>
       <div class="content">
-      <h1 class="title">{{food.name}}</h1>
-      <div class="detail">
-<span class="cell_count">{{food.cellCount}}</span>
-<span class="rating">好评率{{food.rating}}%</span>
-      </div>
-      <div class="price">
-        <span class="now">￥{{food.price}}</span>
-        <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
-      </div>
+        <h1 class="title">{{food.name}}</h1>
+        <div class="detail">
+          <span class="sell_count">月售{{food.sellCount}}份</span><span class="rating">好评率{{food.rating}}%</span>
+        </div>
+        <div class="price">
+          <span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+        </div>
       </div>
     </div>
   </div>
@@ -26,6 +24,8 @@
 </template>
 
 <script type="text/ecmascript-6">
+import BScroll from 'better-scroll';
+
 export default {
   props: {
     food: {
@@ -39,7 +39,19 @@ export default {
   },
   methods: {
     show() {
-      this.showFlag = !this.showFlag;
+      this.showFlag = true;
+      this.$nextTick(() => {
+        if (!this.scroll) {
+          this.scroll = new BScroll(this.$refs.food, {
+            click: true
+          });
+        } else {
+          this.scroll.refresh();
+        }
+      });
+    },
+    hide() {
+      this.showFlag = false;
     }
   }
 };
@@ -60,29 +72,67 @@ export default {
     &.move-leave-active {
         transform: translate3d(100%,0,0);
     }
-.image_header{
-  position: relative;
-  width: 100%;
-  height: 0;
-  padding-bottom: 100%;
-  img{
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-  };
-  .back{
-    position: absolute;
-    top: 10px;
-    left: 0;
-    .icon-arrow_lift{
-      display: block;
-      padding: 8px;
-      font-size: 20px;
-      color: #fff;
+    .image_header {
+        position: relative;
+        width: 100%;
+        height: 0;
+        padding-bottom: 100%;
+        img {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+        };
+        .back {
+            position: absolute;
+            top: 10px;
+            left: 0;
+            .icon-arrow_lift {
+                display: block;
+                padding: 8px;
+                font-size: 20px;
+                color: #fff;
+            }
+        }
     }
-  }
-}
+    .content {
+        padding: 18px;
+        .title {
+            line-height: 14px;
+            margin-bottom: 8px;
+            font-size: 14px;
+            font-weight: 700;
+            color: rgb(7,17,27);
+        };
+        .detail {
+            margin-bottom: 18px;
+            line-height: 10px;
+            font-size: 0;
+            height: 10px;
+            .rating,
+            .sell_count {
+                font-size: 10px;
+                color: rgb(147,153,159);
+            };
+            .sell_count {
+                margin-right: 12px;
+            }
+        };
+        .price {
+            font-weight: 700;
+            line-height: 24px;
+            .now {
+                margin-right: 18px;
+                font-size: 14px;
+                color: rgb(240,20,20);
+            };
+            .old {
+                text-decoration: line-through;
+                font-size: 10px;
+                color: rgb(147,153,159);
+            }
+        };
+    }
 }
 </style>
